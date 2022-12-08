a1f34a30754b3876a88d9fa22c511077d1cf1a19
Sagar Thakur <36344687+Sagyy@users.noreply.github.com>
2022-12-08 12:01:44 +0530

    Add files via upload

---
+++ Day 8/main.js
@@ -0,0 +1,54 @@
+function validTime(str) {
+    //  write code here.
+    const [hours, minutes] = str.split(':').map(time => parseInt(time));
+    return (hours >=0 && hours <=23) && (minutes >=0 && minutes <=59);
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
